504bcc7e2c554db0065d84614bf9b73939a51b0f
ramakanth009 <ramakanthram014@gmail.com>
2023-10-19 10:00:55 +0530

    practice

--- react/hello/src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class Welcome extends Component {
-    render () {
-        return <h1>{this.props.name} a.k.a {this.props.heroname}</h1>
-    }
-}
-
-export default Welcome;
