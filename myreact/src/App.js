eb5d31e9d20c6ca1dcdf924e88944962995315aa
ramakanth009 <ramakanthram014@gmail.com>
2023-11-29 11:40:05 +0530

    practice

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -15,10 +15,14 @@ import Stylesheet from "./components/Stylesheet";
 import Inline from "./components/inline";
 import "./appStyles.css";
 import styles from "./appStyles.moudles.css"
+import Form from "./components/form"
 
 function App() {
   return (
     <div className="App">
+      <Form />
+      {/* <h1 className="error">Error</h1> */}
+      {/* <h1 className={styles.success}>Success</h1> */}
       {/* <Inline /> */}
       {/* <Stylesheet primary={false} /> */}
       {/* <Nameslist /> */}
---
+++ myreact/src/components/form.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+class form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      comments: "",
+      topic: "react",
+    };
+  }
+  handleUsernameChange = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  handleComments = (event) => {
+    this.setState({
+      comments: event.target.value,
+    });
+  };
+  handleTopicChange = (event) => {
+    this.setState({
+      topic: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`);
+    event.preventDefault()
+  };
+  render() {
+    // const {username , comments , topic} = this.state //by this we can remove all three this.state occurances
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>Username : </label>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label>Comments : </label>
+          <textarea
+            type="text"
+            value={this.state.comments}
+            onChange={this.handleComments}
+          ></textarea>
+        </div>
+        <div>
+          <label>Topic : </label>
+          <select value={this.state.topic} onChange={this.handleTopicChange}>
+            <option value="react">React</option>
+            <option value="angular">Angular</option>
+            <option value="vue">Vue</option>
+          </select>
+        </div>
+        <div>
+          <button type= "submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default form;
